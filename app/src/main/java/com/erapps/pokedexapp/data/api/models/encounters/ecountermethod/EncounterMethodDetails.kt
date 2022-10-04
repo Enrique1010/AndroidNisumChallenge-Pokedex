74a0c95b4bcebe47aa16f4af4304f7af6195bbd1
Luis Enrique Ramirez <luis_enrique_ramirez@claro.com.do>
2022-10-04 11:00:18 -0400

    removing encounter method module

--- app/src/main/java/com/erapps/pokedexapp/data/api/models/encounters/ecountermethod/EncounterMethodDetails.kt
+++
@@ -1,8 +0,0 @@
-package com.erapps.pokedexapp.data.api.models.encounters.ecountermethod
-
-data class EncounterMethodDetails(
-    val id: Int,
-    val name: String,
-    val names: List<Name>,
-    val order: Int
-)
--- app/src/main/java/com/erapps/pokedexapp/data/api/models/encounters/ecountermethod/Language.kt
+++
@@ -1,6 +0,0 @@
-package com.erapps.pokedexapp.data.api.models.encounters.ecountermethod
-
-data class Language(
-    val name: String,
-    val url: String
-)
